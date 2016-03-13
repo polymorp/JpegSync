669557c21fb19e5d1246d434fe19a663f04af9b5
Mads Nielsen <post@madsnielsen.dk>
2016-03-13 19:49:46 +0100

    Initial commit

---
+++ JpegSync/JpegInfo.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace JpegSync
+{
+    public class JpegInfo
+    {
+        private string filePath;
+
+        public string FilePath
+        {
+            get
+            {
+                return filePath;
+            }
+            set
+            {
+                filePath = value;
+                try
+                {
+                    size = (new FileInfo(filePath)).Length;
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        public string FileName
+        {
+            get
+            {
+                return Path.GetFileName(filePath);
+            }
+        }
+
+        public long size { get; set; }
+        public bool jpeg { get; set; } = true;
+        public bool error { get; set; } = false;
+        public int width { get; set; }
+        public int height { get; set; }
+
+        public new string ToString
+        {
+            get
+            {
+                var filename = Path.GetFileName(FilePath);
+
+                var fileFormatted =
+                    filename.Length > 50 ?
+                    filename.Substring(0, 23) + "...." + filename.Substring(filename.Length - 23) :
+                    filename.PadLeft(50);
+
+                string info;
+
+                if (!jpeg)
+                    info = "(not a JPEG)";
+                else if (error)
+                    info = "Error reading file!";
+                else
+                    info = String.Format("{0,10} {1,10} bytes", width + "x" + height, size);
+
+                return String.Format("{0}: {1,27}", fileFormatted, info);
+            }
+        }
+
+        public static List<JpegInfo> GetFiles(String path)
+        {
+            var files = new List<JpegInfo>();
+
+            foreach (var file in Directory.EnumerateFiles(path))
+            {
+                JpegBitmapDecoder jpeg;
+
+                try
+                {
+                    var filePath = Path.Combine(path, file);
+
+                    jpeg = new JpegBitmapDecoder(
+                        new FileStream(filePath, FileMode.Open),
+                        System.Windows.Media.Imaging.BitmapCreateOptions.None,
+                        System.Windows.Media.Imaging.BitmapCacheOption.None
+                    );
+
+                    var frame = jpeg.Frames[0];
+                    files.Add(new JpegInfo() { FilePath = file, width = frame.PixelWidth, height = frame.PixelHeight });
+                }
+                catch (FileFormatException)
+                {
+                    files.Add(new JpegInfo() { FilePath = file, jpeg = false });
+                    continue;
+                }
+                catch (Exception)
+                {
+                    files.Add(new JpegInfo() { FilePath = file, error = true });
+                    continue;
+                }
+            }
+
+            return files;
+        }
+
+        public static bool Similar(JpegInfo a, JpegInfo b)
+        {
+            return (
+                a.jpeg == b.jpeg &&
+                a.size == b.size &&
+                a.width == b.width &&
+                a.height == b.height);
+        }
+
+        public bool Similar(JpegInfo compare)
+        {
+            return JpegInfo.Similar(this, compare);
+        }
+    }
+}
---
+++ JpegSync/Program.cs
@@ -0,0 +1,153 @@
+﻿using JpegSync;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace JpegSync
+{
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			var argPointer = 0;
+			String pathInfo;
+
+			while (argPointer < args.Length)
+			{
+				if (args[argPointer].Equals("-i"))
+				{
+					argPointer++;
+					try
+					{
+						// This needs work, the check if the path is valid etc is not robust
+						var expanded = Environment.ExpandEnvironmentVariables(args[argPointer]);
+						argPointer++;
+                        //pathInfo = Path.GetDirectoryName(expanded);
+                        pathInfo = expanded;
+						PrintPathInfo(pathInfo);
+#if DEBUG
+                        Console.ReadKey();
+#endif
+                        return;
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine(args[argPointer] + " is not a valid path");
+						Console.WriteLine();
+						PrintHelpMessage();
+						return;
+					}
+				}
+                else if (args[argPointer].Equals("-s"))
+                {
+                    argPointer++;
+                    try
+                    {
+                        var pathFrom = Environment.ExpandEnvironmentVariables(args[argPointer]);
+                        if (!Directory.Exists(pathFrom))
+                            throw new DirectoryNotFoundException();
+                        argPointer++;
+
+                        var pathTo = Environment.ExpandEnvironmentVariables(args[argPointer]);
+                        if (!Directory.Exists(pathTo))
+                            throw new DirectoryNotFoundException();
+                        argPointer++;
+
+                        SyncPaths(pathFrom, pathTo);
+#if DEBUG
+                        Console.ReadKey();
+#endif
+                        return;
+                    }
+                    catch (DirectoryNotFoundException)
+                    {
+                        Console.WriteLine(args[argPointer] + " is not a valid path");
+                        Console.WriteLine();
+                        PrintHelpMessage();
+                        return;
+                    }
+                }
+				else
+				{
+					PrintHelpMessage();
+					return;
+				}
+			}
+
+			PrintHelpMessage();
+		}
+
+		static void PrintHelpMessage()
+		{
+			Console.WriteLine("Syntax:");
+			Console.WriteLine("JpegSync");
+			Console.WriteLine(" -i path                 Shows info on all JPEG files at the specified location");
+			Console.WriteLine(" -s path_from path_to    Syncs JPEGs from first path to the second");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+#if DEBUG
+            Console.ReadKey();
+#endif
+        }
+
+        static void PrintPathInfo(String pathInfo)
+		{
+			Console.WriteLine("Reading from " + pathInfo);
+
+            var files = JpegInfo.GetFiles(pathInfo);
+
+			foreach (var file in files)
+			{
+                Console.WriteLine(file.ToString);
+            }
+		}
+
+        static void SyncPaths(string pathFrom, string pathTo)
+        {
+            Console.WriteLine("Syncing");
+            Console.WriteLine(String.Format("  {0}", pathFrom.Abbreviate(70, 4)));
+            Console.WriteLine("  to");
+            Console.WriteLine(String.Format("  {0}", pathTo.Abbreviate(70, 4)));
+            Console.WriteLine();
+
+            var filesFrom = JpegInfo.GetFiles(pathFrom).Where(x => x.jpeg && x.width == 1920 && x.height == 1080);
+            var filesTo = JpegInfo.GetFiles(pathTo).Where(x => x.jpeg);
+
+            var filesNew = filesFrom.Where(x => filesTo.All(y => !x.Similar(y)));
+
+            if (filesNew.Count() > 0)
+            {
+                Console.WriteLine("Copying:");
+                foreach (var file in filesNew)
+                {
+                    Console.WriteLine(file.ToString);
+                    var filePathFrom = Path.Combine(pathFrom, file.FileName);
+                    var filePathTo = Path.Combine(pathTo, file.FileName + ".jpg");
+                    File.Copy(filePathFrom, filePathTo);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No new files");
+            }
+            Console.WriteLine();
+        }
+
+        public static string Abbreviate(this string str, int length, int dots)
+        {
+            int left = (length - dots) / 2;
+            int right = (length - dots) - left;
+
+            return
+                str.Length > length ?
+                str.Substring(0, left) + (new string('.', dots)) + str.Substring(str.Length - right) :
+                str;
+        }
+    }
+}
